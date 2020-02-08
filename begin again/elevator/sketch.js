5311f95950ff7d76b123523f04f1f19cd6b6d4b8
Haziq Khalid <zeek@bandlab.com>
2020-02-08 17:59:09 +0800

    update

---
+++ begin again/elevator/sketch.js
@@ -0,0 +1,40 @@
+let e;
+let button;
+let isMoving = false;
+let isMovingB = false;
+let firstFloorLvl = 500;
+let secondFloorLvl = 100;
+let buttonB;
+
+function draw() {
+  background(0);
+  e.display();
+  e.boundary();
+  button.mouseClicked(toggleIsMoving);
+  buttonB.mouseClicked(toggleIsMovingB);
+
+  if (isMoving) {
+    e.moveDown();
+  }
+  
+  if (isMovingB){
+    e.moveUp();
+  }
+}
+
+function setup() {
+  createCanvas(windowWidth,windowHeight);
+  e = new Elevator();
+  button = createButton('first floor');
+  button.position (100, firstFloorLvl);
+  buttonB = createButton('second floor')
+  buttonB.position (100, secondFloorLvl);
+}
+
+function toggleIsMoving() {
+  isMoving = !isMoving;
+}
+
+function toggleIsMovingB() {
+  isMovingB = !isMovingB;
+}
