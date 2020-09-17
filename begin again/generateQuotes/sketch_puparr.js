a48e5644ffdd9d75c48a35c147ad511c8da4621c
Haziq Khalid <zeek@bandlab.com>
2020-09-17 09:28:42 +0800

    changes

--- begin again/generateQuotes/sketch_puparr.js
+++ begin again/generateQuotes/sketch_puparr.js
@@ -1,6 +1,7 @@
 var words = ['l','i', 's', 'p'];
 var words2 = ['c', 'u', 's', 'p'];
 var words3 = ['p', 'u', 'p', 'a'];
+var wordsContain = [];
 var count = 0;
 var count2 = 0;
 var count3 = 0;
@@ -8,33 +9,32 @@ var wordSize = 72;
 var words01;
 
 function preload() {
-  var words01 = loadStrings('words01.txt');
+  //var words01 = loadStrings('words01.txt');
 }
 
 function setup(){
   createCanvas(windowWidth, windowHeight)
+  for(let i = 0; i < 4; i++) {
+    wordsContain.push(new Texter());
+  }
 }
 
 function draw() {
   background(200, 150, 14);
-  textTest();
-  refresh();
+  for (let i = 0; i < wordsContain.length; i++) {
+    wordsContain[i].counter();
+    wordsContain[i].show();
+  }
+  //textTest();
+  //refresh();
 }
 
-
-function textTest() {
-  textSize(wordSize);
-  text(words[count], 100, windowHeight/3);
-  text(words2[count2], 100, windowHeight/3 + wordSize);
-  text(words3[count3], 100, windowHeight/3 + wordSize + wordSize);
-  fill(255);
-}
 
 function refresh() {
-  this.switchInterval = round(random(2,200))
+/*  this.switchInterval = round(random(2,200))
 if (frameCount % this.switchInterval == 0) {
   count = count + 1;
-}
+}*/
 this.switchInterval2 = round(random(2,200))
 if (frameCount % this.switchInterval2 == 0) {
 count2 = count2 + 1;
@@ -53,3 +53,25 @@ count3 = count3 + 1;
     count3 = 0;
   }
 }
+
+class Texter {
+  constructor() {
+    this.x = 100;
+    this.y = windowHeight/3;
+  }
+
+  counter() {
+    this.switchInterval = round(random(2,200))
+  if (frameCount % this.switchInterval == 0) {
+      count = count + 1;
+    }
+  }
+
+  show() {
+    textSize(wordSize);
+    text(words[count], 100, windowHeight/3);
+    fill(255);
+  }
+}
+
+//add for loop in setup
